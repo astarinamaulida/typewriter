5b9de32e2aae6f37579d64df19ddead932c594b4
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-21 18:35:36 -0400

    Add comments

--- typewriter.js
+++ typewriter.js
@@ -5,7 +5,7 @@ for (const char of sentence) {
   //have more control and avoid automatically adding an extra "newline character" at the end each time
   setTimeout(() => {
     process.stdout.write(char); // print the char here
-  }, x); // <= 1s delay to make it noticeable. Can dial it down later.
+  }, x); // 1 second delay
   x += 50;
 }
 setTimeout(() => {
